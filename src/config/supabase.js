b1f90090b52dac9cae3788c0a7ffa453fe0cda5b
nthnco <nathanreso99@gmail.com>
2026-01-22 10:33:24 -0700

    install supabase and add in config

---
+++ src/config/supabase.js
@@ -0,0 +1,15 @@
+import 'react-native-url-polyfill/auto';
+import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const supabaseUrl = '[YOUR_URL]';
+const supabaseAnonKey = '[YOUR_KEY]';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
