e9e083826de1560d22b45d63b4cdacf614742473
nthnco <nathanreso99@gmail.com>
2026-01-22 12:30:20 -0700

    add .env and gitignore and credentiaks

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = '[YOUR_URL]';
-const supabaseAnonKey = '[YOUR_KEY]';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
